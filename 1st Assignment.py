f3a26bafb4a275b5baeac07ebcc0e2e94baeb433
Ghulam Fatima <76777843+Ghulamfatima@users.noreply.github.com>
2020-12-31 07:58:05 +0500

    Add files via upload

---
+++ 1st Assignment.py
@@ -0,0 +1,7 @@
+#Addition , Subtraction , Multiplication , Division:
+a = int(input("Enter first number="))
+b = int(input("Enter second number="))
+print("the addition of those two numbers=",a+b)
+print("the subtraction of those two numbers=",a-b)
+print("the multiplication of those two numbers=",a*b)
+print("the devision of those two numbers=",a/b)
